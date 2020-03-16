6751402a6d6de8f0f8335799160cf1aad69efabe
PieterT2000 <info@codebirds.nl>
2020-03-16 09:47:28 +0100

    Updates

---
+++ precache-manifest.f3b57f8f105e4d9e769b62843ccadcf3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2cef3a03c299808717083c9ebdd68e3",
+    "url": "/vidly/index.html"
+  },
+  {
+    "revision": "617b9f4f5bbdbb3a2efb",
+    "url": "/vidly/static/css/2.c1bb64a5.chunk.css"
+  },
+  {
+    "revision": "7991030295e816ca0c71",
+    "url": "/vidly/static/css/main.ead4fc1b.chunk.css"
+  },
+  {
+    "revision": "617b9f4f5bbdbb3a2efb",
+    "url": "/vidly/static/js/2.d287ff1f.chunk.js"
+  },
+  {
+    "revision": "41136365f4bde441f59a13f97c908c5f",
+    "url": "/vidly/static/js/2.d287ff1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7991030295e816ca0c71",
+    "url": "/vidly/static/js/main.f0112922.chunk.js"
+  },
+  {
+    "revision": "ad7de379d8fc4ca8b448",
+    "url": "/vidly/static/js/runtime-main.d54d515d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vidly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vidly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vidly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vidly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vidly/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
